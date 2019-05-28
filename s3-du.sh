724561cfc5dde051559ae844b6c71904e9ef0ec1
Shlomo Dubrowin <shlomo@dubrowin.org>
2019-05-28 20:52:50 +0300

    Cleanup and comments

--- s3-du.sh
+++ s3-du.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+#########################
+## Compute Prefix (dir) size
+## similar to the Linux du
+## command, but for S3 buckets
+## 
+## By: Shlomo Dubrowin
+## May 28, 2019
+#########################
 
+#########################
+## Functions
+#########################
+
+function Debug {
+        if [ "$DEBUG" == "Y" ]; then
+                echo -e "$1"
+        fi
+}
+
+#########################
 ## CLI Opotions
+#########################
 
 while [ "$1" != "" ]; do
         case $1 in
@@ -16,6 +36,16 @@ while [ "$1" != "" ]; do
         shift
 done
 
+#########################
+## Main Code
+#########################
+
+## If there is no Bucket listed, exit
+if [ -z "$BUCK" ]; then
+        echo -e "\n\tEnter a bucket\n"
+        exit 1
+fi
+
 ## Remove a trailing slash if there is one
 SLASH=`echo $BUCK | rev | cut -c 1 | rev`
 
@@ -23,25 +53,14 @@ if [ "$SLASH" == "/" ]; then
         BUCK=`echo $BUCK | rev | cut -c 2- | rev`
 fi
 
-
-
-if [ -z "$BUCK" ]; then
-        echo -e "\n\tEnter a bucket\n"
-        exit 1
-fi
-
-function Debug {
-        if [ "$DEBUG" == "Y" ]; then
-                echo -e "$1"
-        fi
-}
-
 Debug "Debug: BUCK $BUCK"
 
+## Start Output with Headers
 echo -e "Objects \t Size \t Dir"
 
 Debug "Debug DIR: $( aws s3 ls $BUCK | grep PRE )"
 
+## Loop through the Prefixes (dir) found to produce the sizes
 for DIR in $( aws s3 ls ${BUCK}/ | grep PRE ); do
         if [ "$DIR" != "PRE" ]; then
                 Debug "Debug: aws s3 ls ${BUCK}/${DIR} --recursive --human-readable --summarize"
